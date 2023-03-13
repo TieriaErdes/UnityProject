4cd268121f6c94ac9ac3b7645d2d5daaae8b49e5
TieriaErdes <Amedeo.pro@yandex.com>
2023-03-13 17:18:35 +0300

    Intermidate build 5
    Начало разработки механики инвентаря. Небольшая перестановка в папках скриптов

---
+++ First creative project/Assets/Scripts/inventory scripts/InventorySlot.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
+
+[System.Serializable]
+public class InventorySlot
+{
+    [SerializeField] private Inventory_itemData _itemData;
+    [SerializeField] private int _stackSize;
+
+    public Inventory_itemData ItemData => _itemData;
+    public int StackSize => _stackSize;
+
+    public InventorySlot(Inventory_itemData source, int amount)
+    {
+        _itemData = source;
+        this._stackSize = amount;
+    }
+
+    public InventorySlot()
+    {
+        ClearSlot();
+    }
+
+    public void ClearSlot()
+    {
+        _itemData = null;
+        _stackSize = -1;
+    }
+
+    public bool RoomLeftInStack(int amountToAdd, out int amountRemaining)
+    {
+        amountRemaining = ItemData.MaxStackSize - _stackSize;
+
+        return RoomLeftInStack(amountToAdd);
+    }
+
+    public bool RoomLeftInStack(int amountToAdd)
+    {
+        if (_stackSize + amountToAdd <= _itemData.MaxStackSize)
+            return true;
+        else
+            return false;
+    }
+
+    public void AddToStack(int amount)
+    {
+        _stackSize += amount;
+    }
+
+    public void RemoveFromStack(int amout)
+    {
+        _stackSize -= amout;
+    }
+}
